5a53a509ffa3072ba0cd1b588dcaba5c3ca9ab2b
Marcio S Sartini <msartini@gmail.com>
2015-05-16 15:45:14 -0300

    teste

--- local/tests/ExampleTest.php
+++ local/tests/ExampleTest.php
@@ -2,26 +2,25 @@
 
 use App;
 
-class ExampleTest extends TestCase {
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $response = $this->call('GET', '/');
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
-	public function testBasicExample()
-	{
-		$response = $this->call('GET', '/');
-
-		$this->assertEquals(200, $response->getStatusCode());
-	}
+        $this->assertEquals(200, $response->getStatusCode());
+    }
         
         
-       public function testSoma(){
-           
-           $calculo = new App\Http\Controllers\ProfileController();
-           $this->assertEquals(7, $calculo->soma(3.5, 3.5));
-            
-       } 
-
+    public function testSoma()
+    {
+        $calculo = new App\Http\Controllers\ProfileController();
+        $this->assertEquals(7, $calculo->soma(3.5, 3.5));
+    }
 }
+
