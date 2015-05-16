ea152e2fae33209baa63755ffd1144020a4774ff
Marcio S Sartini <msartini@gmail.com>
2015-05-16 16:23:12 -0300

    migrate

---
+++ database/migrations/2015_05_16_191603_create_estates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstatesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('estates', function(Blueprint $t)
+		{
+                    $t->increments('id');
+
+                    $t->string('name');
+                    $t->string('area')->nullable();
+                    $t->string('bathroom')->nullable();
+                    $t->string('room')->nullable();
+                   
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		 
+            Schema::drop('estates');
+		 
+	}
+
+}
