ed07b5531d6812eb819cca355798f59fb45b0c40
Marcio S Sartini <msartini@gmail.com>
2015-06-06 18:28:50 -0300

    auth state controller

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -13,6 +13,12 @@ use Input;
 class StateController extends Controller
 {
 
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
 
