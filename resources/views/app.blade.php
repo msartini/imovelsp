573f0675397486ec957b5e8a6855580f6da0487d
Marcio S Sartini <msartini@gmail.com>
2015-07-10 00:00:35 -0300

    Adicionado bootstrap compass

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Projeto Márcio</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" ></script>
+
     <script src="http://malsup.github.com/jquery.form.js"></script>
 
     <script src="{{ asset('/js/min/main.min.js') }}"></script>
